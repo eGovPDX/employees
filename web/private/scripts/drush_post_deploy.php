cf004c417a614f4aa3ddb55b3b692b8f8916ee09
Kevin Wang <xinju.wang@portlandoregon.gov>
2021-02-16 15:36:42 -0800

    POWR-3208 Add -y to drush deploy

--- web/private/scripts/drush_post_deploy.php
+++ web/private/scripts/drush_post_deploy.php
@@ -2,5 +2,5 @@
 
 //Clear the cache to make sure database doesn't conflict
 echo "Start running drush deploy...\n";
-passthru('drush deploy -v 2>&1');
+passthru('drush deploy -v -y 2>&1');
 echo "Done running drush deploy.\n";
