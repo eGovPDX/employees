f236f704fccac07db66047477270b430ad5b7cea
Ronnie Mosley <ronnie.j.mosley@gmail.com>
2021-07-14 08:13:57 -0700

    Add bulk actions to content and make adjustments to media and member views

---
+++ web/modules/custom/employees/src/Plugin/Action/UnpublishAction.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Drupal\employees\Plugin\Action;
+
+use Drupal\views_bulk_operations\Action\ViewsBulkOperationsActionBase;
+use Drupal\Core\Session\AccountInterface;
+use Drupal\Core\StringTranslation\StringTranslationTrait;
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * Some description.
+ *
+ * @Action(
+ *   id = "employees_unpublish_action",
+ *   label = @Translation("Unpublish content (custom action)"),
+ *   type = "",
+ *   confirm = FALSE,
+ * )
+ */
+class UnpublishAction extends ViewsBulkOperationsActionBase {
+
+  use StringTranslationTrait;
+
+  /**
+   * {@inheritdoc}
+   */
+  public function execute($entity = NULL) {
+
+    // Get current user's display name
+    $user_display_name = \Drupal::currentUser()->getDisplayName();
+    $entity->status->value = 0;
+    $entity->moderation_state->value="unpublished";
+    $entity->save();
+    // Don't return anything for a default completion message, otherwise return translatable markup.
+    return $this->t('Bulk operation: unpublished by '. $user_display_name);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function access($object, AccountInterface $account = NULL, $return_as_object = FALSE) {
+    if ($object->getEntityType() === 'node' || $object->getEntityType() === 'media') {
+      $access = $object->access('update', $account, TRUE)
+        ->andIf($object->status->access('edit', $account, TRUE));
+      return $return_as_object ? $access : $access->isAllowed();
+    }
+
+    // Other entity types may have different
+    // access methods and properties.
+    return TRUE;
+  }
+}
