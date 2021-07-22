bb025faa348a6507bdf7de87fc7836761a00602f
Kevin Wang <xinju.wang@portlandoregon.gov>
2021-07-22 11:48:41 -0700

    Refactor code to allow the Cron job to update primary groups

--- web/modules/custom/portland_openid_connect/src/Plugin/Action/UpdateUserPrimaryGroups.php
+++ web/modules/custom/portland_openid_connect/src/Plugin/Action/UpdateUserPrimaryGroups.php
@@ -22,72 +22,7 @@ class UpdateUserPrimaryGroups extends ActionBase
    */
   public function execute($account = NULL)
   {
-    // A comma separated string
-    $group_names = $account->field_group_names->value;
-    // Array of unique group names
-    $primary_group_names = [];
-    $new_primary_group_ids = [];
-    if (!empty($group_names)) {
-      // Deduplicate group names
-      if (strpos($group_names, ',') !== false) {
-        $group_name_array = array_map('trim', explode(',', $account->field_group_names->value));
-        $primary_group_names = array_unique($group_name_array);
-        // Update the user's group names field
-        $account->field_group_names->value = implode(',', $primary_group_names);
-      } else {
-        $primary_group_names = [$group_names];
-      }
-    }
-
-    // Build new primary group ID array with primary group names
-    $group_ad_name_list = \Drupal::entityTypeManager()->getStorage('taxonomy_term')
-      ->loadTree('group_ad_name_list', 0, 1, true);
-    $new_primary_group_ids = [];
-    foreach ($group_ad_name_list as $group_ad_name) {
-      if (in_array($group_ad_name->name->value, $primary_group_names)) {
-        $drupal_groups = $group_ad_name->field_employee_groups->getValue();
-        foreach ($drupal_groups as $drupal_group) {
-          $new_primary_group_ids[] = $drupal_group['target_id'];
-        }
-      }
-    }
-    // Remove duplicates
-    $new_primary_group_ids = array_unique($new_primary_group_ids);
-
-    // Build current primary group ID array
-    $current_primary_group_ids = array_map(
-      function($item) { return $item['target_id']; },
-      $account->field_primary_groups->getValue()
-    );
-
-    // Check the new and current group ID arrays and update the user's groups
-    if (empty($new_primary_group_ids)) {
-      // Remove user from all current groups
-      foreach ($current_primary_group_ids as $current_primary_group_id) {
-        UpdateUserPrimaryGroups::removeUserFromGroup($account, $current_primary_group_id);
-      }
-    } else {
-      // For any added group, add membership
-      foreach ($new_primary_group_ids as $new_primary_group_id) {
-        UpdateUserPrimaryGroups::addUserToGroupWithRoles($account, $new_primary_group_id, ['employee-employee']);
-      }
-
-      // Check if any current group is not in the new group list
-      foreach ($current_primary_group_ids as $current_primary_group_id) {
-        if (in_array($current_primary_group_id, $new_primary_group_ids))
-          continue;
-        // Remove user from the new group
-        UpdateUserPrimaryGroups::removeUserFromGroup($account, $current_primary_group_id);
-      }
-
-      // Update the user's primary_groups field
-      $new_primary_groups = [];
-      foreach ($new_primary_group_ids as $new_primary_group_id) {
-        $new_primary_groups[] = ['target_id' => $new_primary_group_id];
-      }
-      $account->field_primary_groups = $new_primary_groups;
-      $account->save();
-    }
+    UpdateUserPrimaryGroups::updatePrimaryGroupsForUser($account);
   }
 
   /**
@@ -139,5 +74,69 @@ class UpdateUserPrimaryGroups extends ActionBase
     $group = \Drupal\group\Entity\Group::load($group_id);
     $group->removeMember($account);
     $group->save();
+  }
+
+  /**
+   * Helper function to remove a user from a group.
+   * Will be called by User post-save hooks and this view bulk operation.
+   */
+  public static function updatePrimaryGroupsForUser($account) {
+    if(empty($account)) return;
+    // A comma separated string
+    $group_names = $account->field_group_names->value;
+    // Array of unique group names
+    $primary_group_names = [];
+    $new_primary_group_ids = [];
+    if (!empty($group_names)) {
+      // Deduplicate group names
+      if (strpos($group_names, ',') !== false) {
+        $group_name_array = array_map('trim', explode(',', $account->field_group_names->value));
+        $primary_group_names = array_unique($group_name_array);
+      } else {
+        $primary_group_names = [$group_names];
+      }
+    }
+
+    // Build new primary group ID array with primary group names
+    $group_ad_name_list = \Drupal::entityTypeManager()->getStorage('taxonomy_term')
+      ->loadTree('group_ad_name_list', 0, 1, true);
+    $new_primary_group_ids = [];
+    foreach ($group_ad_name_list as $group_ad_name) {
+      if (in_array($group_ad_name->name->value, $primary_group_names)) {
+        $drupal_groups = $group_ad_name->field_employee_groups->getValue();
+        foreach ($drupal_groups as $drupal_group) {
+          $new_primary_group_ids[] = $drupal_group['target_id'];
+        }
+      }
+    }
+    // Remove duplicates
+    $new_primary_group_ids = array_unique($new_primary_group_ids);
+
+    // Build current primary group ID array
+    $current_primary_group_ids = array_map(
+      function($item) { return $item['target_id']; },
+      $account->field_primary_groups->getValue()
+    );
+
+    // Check the new and current group ID arrays and update the user's groups
+    if (empty($new_primary_group_ids)) {
+      // Remove user from all current groups
+      foreach ($current_primary_group_ids as $current_primary_group_id) {
+        UpdateUserPrimaryGroups::removeUserFromGroup($account, $current_primary_group_id);
+      }
+    } else {
+      // For any added group, add membership
+      foreach ($new_primary_group_ids as $new_primary_group_id) {
+        UpdateUserPrimaryGroups::addUserToGroupWithRoles($account, $new_primary_group_id, ['employee-employee']);
+      }
+
+      // Check if any current group is not in the new group list
+      foreach ($current_primary_group_ids as $current_primary_group_id) {
+        if (in_array($current_primary_group_id, $new_primary_group_ids))
+          continue;
+        // Remove user from the new group
+        UpdateUserPrimaryGroups::removeUserFromGroup($account, $current_primary_group_id);
+      }
+    }
   }
 }
