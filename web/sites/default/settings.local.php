fa304ee12248375c0053f3b85237a807e53ff285
Josh Mitchell <joshuami@users.noreply.github.com>
2022-01-13 15:17:03 -0800

    POWR-3939 attempt at getting a local solr server connected.

--- web/sites/default/settings.local.php
+++ web/sites/default/settings.local.php
@@ -163,3 +163,8 @@ $settings['trusted_host_patterns'] = array(
 );
 
 $settings['file_temp_path'] = '/tmp';
+
+// SOLR on Pantheon defines the scheme as an environment variable.
+// We define this just so that the module doesn't break.
+putenv('PANTHEON_INDEX_SCHEME=https');
+putenv('PANTHEON_INDEX_PORT=449');
