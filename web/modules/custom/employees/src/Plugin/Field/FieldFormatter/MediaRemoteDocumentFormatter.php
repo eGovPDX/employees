c5b017e117a246350da42fe224ce4436d78de05c
Josh Mitchell <joshuami@users.noreply.github.com>
2022-04-27 15:39:21 -0700

    POWR-4183 rebase master.

---
+++ web/modules/custom/employees/src/Plugin/Field/FieldFormatter/MediaRemoteDocumentFormatter.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace Drupal\employees\Plugin\Field\FieldFormatter;
+
+use Drupal\Core\Field\FieldItemListInterface;
+use Drupal\media_remote\Plugin\Field\FieldFormatter\MediaRemoteFormatterBase;
+
+/**
+ * Plugin implementation of the 'media_remote_document' formatter.
+ *
+ * @FieldFormatter(
+ *   id = "media_remote_document",
+ *   label = @Translation("Remote Media - Document"),
+ *   field_types = {
+ *     "string"
+ *   }
+ * )
+ */
+class MediaRemoteDocumentFormatter extends MediaRemoteFormatterBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function getUrlRegexPattern() {
+    return '/^https:\/\/efiles\.portlandoregon\.gov\/Record\/([\d]+)(\/)?/';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function getValidUrlExampleStrings(): array {
+    return [
+      'https://efiles.portlandoregon.gov/Record/4105477/',
+      'https://efiles.portlandoregon.gov/Record/4105477/File/Document',
+    ];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function deriveMediaDefaultNameFromUrl($url) {
+    $matches = [];
+    // DocumentCloud URLs can either contain only Document ID, or
+    // have a text-string suffix. If there is a suffix, we'll use that
+    // as the name. Otherwise, we'll return a generic name with the
+    // document ID.
+    $pattern = static::getUrlRegexPattern();
+    preg_match($pattern, $url, $matches);
+    if (!empty($matches[1])) {
+      return $matches[1];
+    }
+    return parent::deriveMediaDefaultNameFromUrl($url);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function viewElements(FieldItemListInterface $items, $langcode) {
+    $elements = [];
+    foreach ($items as $delta => $item) {
+      /** @var \Drupal\Core\Field\FieldItemInterface $item */
+      if ($item->isEmpty()) {
+        continue;
+      }
+      $matches = [];
+      $pattern = static::getUrlRegexPattern();
+      preg_match($pattern, $item->value, $matches);
+      if (empty($matches[1])) {
+        continue;
+      }
+      $document_id = $matches[1];
+      $elements[$delta] = [
+        '#theme' => 'media_remote_document',
+        '#document_id' => $document_id,
+        // '#slug' => $matches[2][0] ?? '',
+      ];
+    }
+    return $elements;
+  }
+
+}
