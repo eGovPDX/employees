b180ed5fc3d2c5556cd3bfb0e03f0ee9e8e27a21
Ronnie Mosley <34037881+radmosley@users.noreply.github.com>
2022-05-11 16:45:03 -0700

    POWR-4179 Add Percy visual regression testing. (#288)
    * powr-4179 Add yarn and jest commands, initiate package.json
    
    * powr-4179 add jest commands to package.json
    
    * powr-4179 update circle ci config with visual regression testing
    
    * powr-4179 update circleci/config.js
    
    * powr-4279 Update circleci/config.js
    
    * update small change
    
    * powr-4179 update name to test
    
    * ci config update
    
    * powr-4179 update site name in config.yml
    
    * update site name in percy test and package.json
    
    * powr-4179 update circle ci config
    
    * Updated config.yml
    
    * POWR-4179 Revert yarn.lock to PortlandOR
    
    * POWR-4179 Run "all-tests"
    
    * POWr-4179 Add login
    
    * POWR-4179 Run single test
    
    * POWR-4179 Test skipping percy finalize_all
    
    * powr-4179 Group Home Page snapshot
    
    * powr-4179 add search view tests
    
    * powr-4179 Add news groups and event list pages snapshots
    
    * Update readme change news and events path
    
    * powr-4179 update old theme reference to Westy
    
    Co-authored-by: kxwang <xinju.wang@portlandoregon.gov>

---
+++ tests/percy/__tests__/admin.js
@@ -0,0 +1,129 @@
+const percySnapshot = require("@percy/puppeteer");
+const puppeteer = require("puppeteer");
+
+const SITE_NAME = process.env.SITE_NAME;
+const PROJECT_NAME = process.env.CIRCLE_PROJECT_REPONAME
+const HOME_PAGE = SITE_NAME
+    ? `https://${SITE_NAME}-${PROJECT_NAME}.pantheonsite.io`
+    : "https://employees.lndo.site";
+const ARTIFACTS_FOLDER = (SITE_NAME) ? `/home/circleci/artifacts/` : `./`;
+
+let BROWSER_OPTION = {
+    ignoreHTTPSErrors: true,
+    args: ["--no-sandbox"],
+    defaultViewport: null,
+};
+
+describe('Visual Regression Testing', () => {
+    let browser, page;
+    beforeAll(async () => {
+        browser = await puppeteer.launch(BROWSER_OPTION);
+        page = await browser.newPage();
+
+        if (process.env.CIRCLECI) {
+            // On CI, the CI script will call terminus to retrieve login URL
+            login_url = process.env.SUPERADMIN_LOGIN;
+            await page.goto(login_url);
+        }
+        else {
+            var drush_uli_result = fs.readFileSync("superAdmin_uli.log").toString();
+            login_url = drush_uli_result.replace('http://default', 'https://portlandor.lndo.site');
+            // Log in once for all tests to save time
+            await page.goto(login_url);
+        }
+    })
+
+    afterAll(async () => {
+        await browser.close();
+    })
+    // Home Page
+    it('Home Page', async function () {
+        try {
+            await page.goto(HOME_PAGE)
+            await percySnapshot(page, "SuperAdmin - Home Page")
+        } catch (e) {
+            // Capture the screenshot when test fails and re-throw the exception
+            await page.screenshot({
+                path: `${ARTIFACTS_FOLDER}home-page-error.jpg`,
+                type: "jpeg",
+                fullPage: true
+            });
+            throw e;
+        }
+    })
+    // Group Home Page
+    it('Group Home Page', async function () {
+        try {
+            await page.goto(`${HOME_PAGE}/web-support`)
+            await percySnapshot(page, "SuperAdmin - City Web Editors")
+        } catch (e) {
+            // Capture the screenshot when test fails and re-throw the exception
+            await page.screenshot({
+                path: `${ARTIFACTS_FOLDER}group-page-error.jpg`,
+                type: "jpeg",
+                fullPage: true
+            });
+            throw e;
+        }
+    })
+    // Directory Search View
+    it('Directory Search View', async function () {
+        try {
+            await page.goto(`${HOME_PAGE}/directory`)
+            await percySnapshot(page, "SuperAdmin - Directory")
+        } catch (e) {
+            // Capture the screenshot when test fails and re-throw the exception
+            await page.screenshot({
+                path: `${ARTIFACTS_FOLDER}directory-page-error.jpg`,
+                type: "jpeg",
+                fullPage: true
+            });
+            throw e;
+        }
+    })
+    // News Search View
+    it('News Search View', async function () {
+        try {
+            await page.goto(`${HOME_PAGE}/news`)
+            await percySnapshot(page, "SuperAdmin - News")
+        } catch (e) {
+            // Capture the screenshot when test fails and re-throw the exception
+            await page.screenshot({
+                path: `${ARTIFACTS_FOLDER}powr-news-error.jpg`,
+                type: "jpeg",
+                fullPage: true
+            });
+            throw e;
+        }
+    })
+    // Events Search View
+    it('Events Search View', async function () {
+        try {
+            await page.goto(`${HOME_PAGE}/events`)
+            await percySnapshot(page, "SuperAdmin - Events")
+        } catch (e) {
+            // Capture the screenshot when test fails and re-throw the exception
+            await page.screenshot({
+                path: `${ARTIFACTS_FOLDER}powr-events-error.jpg`,
+                type: "jpeg",
+                fullPage: true
+            });
+            throw e;
+        }
+    })
+    // Groups Search View
+    it('Groups', async function () {
+        try {
+            await page.goto(`${HOME_PAGE}/groups`)
+            await percySnapshot(page, "SuperAdmin - Groups")
+        } catch (e) {
+            // Capture the screenshot when test fails and re-throw the exception
+            await page.screenshot({
+                path: `${ARTIFACTS_FOLDER}groups-page-error.jpg`,
+                type: "jpeg",
+                fullPage: true
+            });
+            throw e;
+        }
+    })
+})
