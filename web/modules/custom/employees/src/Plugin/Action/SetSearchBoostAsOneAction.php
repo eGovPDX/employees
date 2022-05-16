ea2909e74db8a3f761cbfcf0dd5704514e0a8a4b
Kevin Wang <xinju.wang@portlandoregon.gov>
2022-05-16 10:46:13 -0700

    POWR-4237 Add a search boost field to User

---
+++ web/modules/custom/employees/src/Plugin/Action/SetSearchBoostAsOneAction.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Drupal\employees\Plugin\Action;
+
+use Drupal\views_bulk_operations\Action\ViewsBulkOperationsActionBase;
+use Drupal\Core\Session\AccountInterface;
+use Drupal\Core\StringTranslation\StringTranslationTrait;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Access\AccessResult;
+
+/**
+ * Some description.
+ *
+ * @Action(
+ *   id = "employees_set_search_boost_as_one_action",
+ *   label = @Translation("Set search boost as 1 (custom action)"),
+ *   type = "user",
+ *   confirm = FALSE,
+ * )
+ */
+class SetSearchBoostAsOneAction extends ViewsBulkOperationsActionBase {
+
+  use StringTranslationTrait;
+
+  /**
+   * {@inheritdoc}
+   */
+  public function execute($entity = NULL) {
+    // Get current user's display name
+    $user_display_name = \Drupal::currentUser()->getDisplayName();
+    if(!$entity->hasField('field_search_boost'))
+      return $this->t('Does not have field_search_boost.');
+
+    $entity->field_search_boost->value = 1.0;
+    $entity->save();
+
+    // Don't return anything for a default completion message, otherwise return translatable markup.
+    return $this->t('Bulk operation: search boost set as 1 by '. $user_display_name);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function access($object, AccountInterface $account = NULL, $return_as_object = FALSE) {
+    if ($object->getEntityTypeId() === 'node' || $object->getEntityTypeId() === 'media') {
+      $access = $object->access('update', $account, TRUE)->andIf($object->access('edit', $account, TRUE));
+      return $return_as_object ? $access : $access->isAllowed();
+    }
+
+    // Other entity types may have different
+    // access methods and properties.
+    return ($return_as_object ? AccessResult::allowed() : true );
+  }
+}
