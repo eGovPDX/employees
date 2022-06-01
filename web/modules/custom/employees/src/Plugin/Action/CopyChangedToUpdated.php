0c9ce49eaf8cb045aa38cb92ebee7ca0d1954fa4
Kevin Wang <xinju.wang@portlandoregon.gov>
2022-06-01 14:59:15 -0700

    POWR-4255 Add sleep time

--- web/modules/custom/employees/src/Plugin/Action/CopyChangedToUpdated.php
+++ web/modules/custom/employees/src/Plugin/Action/CopyChangedToUpdated.php
@@ -26,19 +26,21 @@ class CopyChangedToUpdated extends ViewsBulkOperationsActionBase {
    * {@inheritdoc}
    */
   public function execute($entity = NULL) {
+    try{
+
     // Only process entity that has "field_updated_on"
     if(! $entity->hasField('field_updated_on')) return $this->t('Bulk operation: the entity does not have an Updated On field');
 
     $entity->field_updated_on->value = \Drupal::service('date.formatter')->format($entity->changed->value, 'local_datetime', '', 'UTC');
-    try{
     $entity->save();
-    } catch (Exception $e) {
-      \Drupal::logger('php')->error('Error saving entity ' . $entity->id() . ": " . $e->getTraceAsString());
-    }
-    sleep(1); // Add a delay between saves
+    sleep(5); // Add a delay between saves
 
     // Don't return anything for a default completion message, otherwise return translatable markup.
     return $this->t('Bulk operation: copied date from Changed to Updated On field');
+
+  } catch (Exception $e) {
+    \Drupal::logger('php')->error('Error saving entity ' . $entity->id() . ": " . $e->getTraceAsString());
+  }
   }
 
   /**
