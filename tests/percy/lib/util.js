b06294d992cadf016016ec67098a2e0deaaee98c
Ronnie Mosley <ronnie.j.mosley@gmail.com>
2022-07-07 11:23:18 -0700

    pe-275 util.js

--- tests/percy/lib/util.js
+++ tests/percy/lib/util.js
@@ -2,5 +2,6 @@ exports.removeEnvironmentIndicator = async function (page) {
     await page.evaluate(
         () => {
           document.querySelector("#toolbar-item-environment-indicator").innerText = ""
+          
         })
 }
