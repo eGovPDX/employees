006703a509e17cdd80284ed394c2af30706567ff
Ronnie Mosley <ronnie.j.mosley@gmail.com>
2022-08-10 10:52:14 -0700

    pe-313 add back to top button and styling

--- web/themes/custom/westy/src/js/back-to-top.js
+++ web/themes/custom/westy/src/js/back-to-top.js
@@ -1,22 +1,23 @@
-// (function ($, Drupal) {
+(function ($, Drupal) {
 
-  // Drupal.behaviors.westyBackToTop = {
-  //   attach: function (context, settings) {
-  //     var viewHeight = $(window).height();
-  //     var showHeight = viewHeight * .75;
-  //     var isAttached = false;
+  Drupal.behaviors.westyBackToTop = {
+    attach: function (context, settings) {
+      var viewHeight = $(window).height();
+      var showHeight = viewHeight * .75;
+      var isAttached = false;
 
-  //     $(window).once('backToTopShowButtonHandler').on('scroll', function () {
-  //       var scrollPos = $(document).scrollTop();
-  //       if (scrollPos > showHeight && !isAttached) {
-  //         var buttonText = Drupal.t('Back to top');
-  //         $('.layout__region--main', context).append(`<div id="back-to-top" class="btn btn-dark position-fixed bottom-50 end-0 zindex-popover"><a href=".page__content">${buttonText}</a></div>`);
-  //         isAttached = true;
-  //       } else if (scrollPos <= showHeight && isAttached) {
-  //         $('#back-to-top').remove();
-  //         isAttached = false;
-  //       }
-  //     });
-  //   }
-  // }
-// })(jQuery, Drupal);
+      $(window).once('backToTopShowButtonHandler').on('scroll', function () {
+        var scrollPos = $(document).scrollTop();
+        if (scrollPos > showHeight && !isAttached) {
+          var buttonText = Drupal.t('Back to top');
+          // $('.layout__region--main', context).append(`<div id="back-to-top" class="btn btn-lg btn-dark zindex-popover position-fixed bottom-0 end-50 mb-3 me-3"><a class="link-light" href="#header">${buttonText}</a></div>`);
+          $('.layout__region--main', context).append(`<div id="back-to-top" class="btn btn-lg btn-dark zindex-popover position-fixed bottom-0 end-0 mb-3 me-3"><a class="link-light" href="#header">${buttonText}</a></div>`);
+          isAttached = true;
+        } else if (scrollPos <= showHeight && isAttached) {
+          $('#back-to-top').remove();
+          isAttached = false;
+        }
+      });
+    }
+  }
+})(jQuery, Drupal);
--- web/themes/custom/westy/src/js/westy.script.js
+++ web/themes/custom/westy/src/js/westy.script.js
@@ -15,27 +15,6 @@ import '../components/nav/_nav';
 (function ($, Drupal) {
 
   'use strict';
-  Drupal.behaviors.westyBackToTop = {
-    attach: function (context, settings) {
-      var viewHeight = $(window).height();
-      //viewHeight is showing the entire pages height
-      var showHeight = viewHeight * .75;
-      var isAttached = false;
-
-      $(window).once('backToTopShowButtonHandler').on('scroll', function () {
-        var scrollPos = $(document).scrollTop();
-        if (scrollPos > showHeight && !isAttached) {
-          var buttonText = Drupal.t('Back to top');
-          //Needs margin
-          $('.layout__region--main', context).append(`<div id="back-to-top" class="btn btn-dark position-fixed bottom-50 end-0 zindex-popover"><a href=".page__content">${buttonText}</a></div>`);
-          isAttached = true;
-        } else if (scrollPos <= showHeight && isAttached) {
-          $('#back-to-top').remove();
-          isAttached = false;
-        }
-      });
-    }
-  }
   // Drupal.behaviors.westyBackToTop = {
   //   attach: function (context, settings) {
   //     var viewHeight = $(window).height();
