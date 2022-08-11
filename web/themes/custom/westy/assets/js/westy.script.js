2166ae8a268cc90758aa18d1944b722ba74f1354
Ronnie Mosley <ronnie.j.mosley@gmail.com>
2022-08-11 08:59:31 -0700

    pe-313 inline styling for back to top button

--- web/themes/custom/westy/assets/js/westy.script.js
+++ web/themes/custom/westy/assets/js/westy.script.js
@@ -3377,7 +3377,7 @@ __webpack_require__.r(__webpack_exports__);
         if (scrollPos > showHeight && !isAttached) {
           var buttonText = Drupal.t('Back to top'); // $('.layout__region--main', context).append(`<div id="back-to-top" class="btn btn-lg btn-dark zindex-popover position-fixed bottom-0 end-50 mb-3 me-3"><a class="link-light" href="#header">${buttonText}</a></div>`);
 
-          $('.layout__region--main', context).append("<div id=\"back-to-top\" class=\"btn btn-lg btn-dark zindex-popover position-fixed bottom-0 end-0 mb-3 me-3\"><a class=\"link-light\" href=\"#header\">".concat(buttonText, "</a></div>"));
+          $('.layout__region--main', context).append("<div id=\"back-to-top\" class=\"btn btn-lg btn-dark zindex-popover position-fixed bottom-0 end-0 mb-3\" style=\"margin: 9rem;\"><a class=\"link-light\" href=\"#header\">".concat(buttonText, "</a></div>"));
           isAttached = true;
         } else if (scrollPos <= showHeight && isAttached) {
           $('#back-to-top').remove();
