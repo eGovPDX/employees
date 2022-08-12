ed65d1e099ef6eb1e83be2d2b366e5b81b3c255a
Ronnie Mosley <ronnie.j.mosley@gmail.com>
2022-08-12 11:38:55 -0700

    pe-313 add back-to-top.scss

--- web/themes/custom/westy/src/js/back-to-top.js
+++ web/themes/custom/westy/src/js/back-to-top.js
@@ -10,7 +10,7 @@
         var scrollPos = $(document).scrollTop();
         if (scrollPos > showHeight && !isAttached) {
           var buttonText = Drupal.t('Back to top');
-          $('.layout__region--main', context).append(`<div id="back-to-top" class="btn btn-lg btn-dark zindex-popover position-fixed bottom-0 end-0 mb-3"><a class="link-light" href="#header">${buttonText}</a></div>`);
+          $('.layout__region--main', context).append(`<div id="back-to-top" class="btn btn-lg btn-dark"><a class="link-light" href="#header">${buttonText}</a></div>`);
           isAttached = true;
         } else if (scrollPos <= showHeight && isAttached) {
           $('#back-to-top').remove();
