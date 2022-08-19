bacb1cfa615e347ef19f5958b56d9c969bb621c7
Ronnie Mosley <ronnie.j.mosley@gmail.com>
2022-08-19 15:33:06 -0700

    pe-313 remove underline from back to top button

--- web/themes/custom/westy/src/js/back-to-top.js
+++ web/themes/custom/westy/src/js/back-to-top.js
@@ -10,7 +10,7 @@
         var scrollPos = $(document).scrollTop();
         if (scrollPos > showHeight && !isAttached) {
           var buttonText = Drupal.t('Back to top');
-          $('.layout__region--main', context).append(`<div id="back-to-top" class="btn btn-lg btn-dark"><a class="link-light" href="#header">${buttonText}</a></div>`);
+          $('.layout__region--main', context).append(`<div id="back-to-top" class="btn btn-lg btn-dark"><a class="link-light text-decoration-none" href="#header">${buttonText}</a></div>`);
           isAttached = true;
         } else if (scrollPos <= showHeight && isAttached) {
           $('#back-to-top').remove();
