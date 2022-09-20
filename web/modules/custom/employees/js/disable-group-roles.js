6ce468923823089d4eb762bb6ad31a32d3300a4b
kxwang <xinju.wang@portlandoregon.gov>
2022-09-19 17:21:34 -0700

    PE-371 Group users can't be both editor and admin (#385)
    * PE-371 Group users can't be both Editor and Admin
    
    * PE-371 Remove redundant Member role

--- web/modules/custom/employees/js/disable-group-roles.js
+++ web/modules/custom/employees/js/disable-group-roles.js
@@ -1,3 +1,42 @@
+// Ensure that the user only gets either Admin or Editor role
+function uncheckAdminOrEditor(event) {
+  switch(event.target.id) {
+    case "edit-group-roles-employee-admin":
+      document.getElementById("edit-group-roles-employee-editor").checked = false;
+      break;
+    case "edit-group-roles-employee-editor":
+      document.getElementById("edit-group-roles-employee-admin").checked = false;
+      break;
+    case "edit-group-roles-private-admin":
+      document.getElementById("edit-group-roles-private-editor").checked = false;
+      break;
+    case "edit-group-roles-private-editor":
+      document.getElementById("edit-group-roles-private-admin").checked = false;
+      break;
+  }
+}
+
 (function($){
-  $("div.form-item--group-roles-private-employee").hide();
+  if(! drupalSettings.isAdmin) {
+    $("div.form-item--group-roles-private-employee").hide();
+    $("div.form-item--group-roles-private-following").hide();
+    $("div.form-item--group-roles-employee-employee").hide();
+    $("div.form-item--group-roles-employee-following").hide();
+  }
+
+  var adminCheckbox = document.getElementById("edit-group-roles-employee-admin");
+  if(adminCheckbox)
+    adminCheckbox.addEventListener('click', uncheckAdminOrEditor);
+  
+  var editorCheckbox = document.getElementById("edit-group-roles-employee-editor");
+  if(editorCheckbox)
+    editorCheckbox.addEventListener('click', uncheckAdminOrEditor);
+
+  adminCheckbox = document.getElementById("edit-group-roles-private-admin");
+  if(adminCheckbox)
+    adminCheckbox.addEventListener('click', uncheckAdminOrEditor);
+  
+  editorCheckbox = document.getElementById("edit-group-roles-private-editor");
+  if(editorCheckbox)
+    editorCheckbox.addEventListener('click', uncheckAdminOrEditor);
 })(jQuery);
