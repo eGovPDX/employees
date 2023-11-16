46855a536b9642520838170d955b24f53f639276
Kevin Wang <xinju.wang@portlandoregon.gov>
2023-11-15 16:44:00 -0800

    PE-549 Update custom code for user roles

--- web/modules/custom/employees/js/disable-group-roles.js
+++ web/modules/custom/employees/js/disable-group-roles.js
@@ -1,17 +1,29 @@
-// Ensure that the user only gets either Admin or Editor role
-function uncheckAdminOrEditor(event) {
+// Ensure that the user only gets one of Admin, Editor, or Reviewer roles
+function enforceUserRoleSelection(event) {
   switch(event.target.id) {
     case "edit-group-roles-employee-admin":
       document.getElementById("edit-group-roles-employee-editor").checked = false;
+      document.getElementById("edit-group-roles-employee-reviewer").checked = false;
       break;
     case "edit-group-roles-employee-editor":
       document.getElementById("edit-group-roles-employee-admin").checked = false;
+      document.getElementById("edit-group-roles-employee-reviewer").checked = false;
+      break;
+    case "edit-group-roles-employee-reviewer":
+      document.getElementById("edit-group-roles-employee-admin").checked = false;
+      document.getElementById("edit-group-roles-employee-editor").checked = false;
       break;
     case "edit-group-roles-private-admin":
       document.getElementById("edit-group-roles-private-editor").checked = false;
+      document.getElementById("edit-group-roles-private-reviewer").checked = false;
       break;
     case "edit-group-roles-private-editor":
       document.getElementById("edit-group-roles-private-admin").checked = false;
+      document.getElementById("edit-group-roles-private-reviewer").checked = false;
+      break;
+    case "edit-group-roles-private-reviewer":
+      document.getElementById("edit-group-roles-private-admin").checked = false;
+      document.getElementById("edit-group-roles-private-editor").checked = false;
       break;
   }
 }
@@ -26,17 +38,25 @@ function uncheckAdminOrEditor(event) {
 
   var adminCheckbox = document.getElementById("edit-group-roles-employee-admin");
   if(adminCheckbox)
-    adminCheckbox.addEventListener('click', uncheckAdminOrEditor);
+    adminCheckbox.addEventListener('click', enforceUserRoleSelection);
   
   var editorCheckbox = document.getElementById("edit-group-roles-employee-editor");
   if(editorCheckbox)
-    editorCheckbox.addEventListener('click', uncheckAdminOrEditor);
+    editorCheckbox.addEventListener('click', enforceUserRoleSelection);
+
+  var editorCheckbox = document.getElementById("edit-group-roles-employee-reviewer");
+  if(editorCheckbox)
+    editorCheckbox.addEventListener('click', enforceUserRoleSelection);
 
   adminCheckbox = document.getElementById("edit-group-roles-private-admin");
   if(adminCheckbox)
-    adminCheckbox.addEventListener('click', uncheckAdminOrEditor);
+    adminCheckbox.addEventListener('click', enforceUserRoleSelection);
   
   editorCheckbox = document.getElementById("edit-group-roles-private-editor");
   if(editorCheckbox)
-    editorCheckbox.addEventListener('click', uncheckAdminOrEditor);
+    editorCheckbox.addEventListener('click', enforceUserRoleSelection);
+  
+  editorCheckbox = document.getElementById("edit-group-roles-private-reviewer");
+  if(editorCheckbox)
+    editorCheckbox.addEventListener('click', enforceUserRoleSelection);
 })(jQuery);
