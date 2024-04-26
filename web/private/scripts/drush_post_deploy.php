dacf76981c95c5ea775e6def7c14cb5b616eda07
Richard Davies <richard.davies@portlandoregon.gov>
2024-04-26 08:30:50 -0700

    PE-601 Post custom Solr schema during test & live deploys

--- web/private/scripts/drush_post_deploy.php
+++ web/private/scripts/drush_post_deploy.php
@@ -1,6 +1,8 @@
 <?php
 
-//Clear the cache to make sure database doesn't conflict
 echo "Start running drush deploy...\n";
 passthru('drush deploy -v -y 2>&1');
 echo "Done running drush deploy.\n";
+
+echo "Post our custom Solr schema to Pantheon's Solr server...\n";
+passthru('drush search-api-pantheon:postSchema pantheon_solr8 ../private/solr-conf 2>&1');
