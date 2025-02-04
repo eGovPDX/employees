80fcda00261ef7aa41951c6122ddd127c2421d96
kxwang <xinju.wang@portlandoregon.gov>
2025-02-04 15:34:25 -0800

    PE-677 New page menu implementation with group_content_menu (#638)
    * PE-662 Fix the performance issue on page menu
    
    * Add config changes
    
    * Update composer.lock
    
    * Update twigs and view config
    
    * Add migration Drush command
    
    * Configure permissions. Remove unused view.
    
    * Add the Manage Menu button
    
    * Automatically update aliases. Support 7 level menu
    
    * Update composer.lock after merging master
    
    * Add custom entity reference selection to filter by the group ID in the group menu path
    
    * Add validation to prevent duplicated entries in Page Menu
    
    * Add "/edit" to Page Menu breadcrumb link
    
    * Remove "Menu" from Page and Resource forms
    
    * Use regex to match Page Menu link
    
    * Display page menu link description when hovering
    
    * Re-generate hash after merging with master
    
    * Hide the Menu Settings block in node form
    
    * FIx test and revert DBLog settings
    
    * Revert the update in util.js
    
    * Validate links entered as strings
    
    * Fix a bug in validating URI

--- web/modules/custom/portland_groups/src/Controller/PortlandGroupRelationshipController.php
+++ web/modules/custom/portland_groups/src/Controller/PortlandGroupRelationshipController.php
@@ -14,6 +14,7 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
 use Drupal\group\Entity\GroupRelationshipType;
 use Drupal\node\Entity\NodeType;
 use Drupal\media\Entity\MediaType;
+use Drupal\group_content_menu\Entity\GroupContentMenuType;
 
 use Drupal\Core\Config\Entity;
 use Symfony\Component\HttpKernel;
@@ -95,6 +96,9 @@ class PortlandGroupRelationshipController extends GroupRelationshipController {
                 break;
             case "node":
                 $content_type = NodeType::load($plugin_bundle);
+                break;
+            case "group_content_menu":
+                $content_type = GroupContentMenuType::load($plugin_bundle);
                 break;
             default:
                 $content_type = "undefined";
