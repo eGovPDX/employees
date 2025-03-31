4eb40a3f31f9c14f11eff321ea2c9886d551347a
Richard Davies <richard.davies@portlandoregon.gov>
2025-03-31 12:58:09 -0700

    PE-692 Bulk publish action doesn't check content moderation permissions (#656)

--- web/modules/custom/employees/src/Plugin/Action/UnpublishAction.php
+++ web/modules/custom/employees/src/Plugin/Action/UnpublishAction.php
@@ -41,12 +41,38 @@ class UnpublishAction extends ViewsBulkOperationsActionBase {
    */
   public function access($object, AccountInterface $account = NULL, $return_as_object = FALSE) {
     if ($object->getEntityTypeId() === 'node' || $object->getEntityTypeId() === 'media') {
-      $access = $object->access('update', $account, TRUE);
+      $moderation_info = \Drupal::service('content_moderation.moderation_information');
+
+      // For moderated content, edit access to the entity is not enough. 
+      // Users should also have permission to the proper workflow transition to unpublish moderated content.
+      if ($moderation_info->isModeratedEntity($object)) {
+        // Find the workflow transition to unpublished state and get the workflow ID and transition ID
+        // so we can build the permission name to check.
+        $workflow_id = $moderation_info->getWorkflowForEntity($object)->get('id');
+        $transitions = $moderation_info->getWorkflowForEntity($object)->get('type_settings')['transitions'];
+        $transition_id = null;
+        foreach ($transitions as $key => $transition) {
+          if ($transition['to'] == 'unpublished') {
+            $transition_id = $key;
+            break;
+          }
+        }
+        if ($transition_id == null) {
+          // No transition to unpublished state found.
+          return ($return_as_object ? AccessResult::forbidden() : false );
+        }
+
+        // The workflow transition permission is named like "use editorial transition unpublish"
+        $access = $object->access('update', $account, TRUE)
+          ->andIf(AccessResult::allowedIfHasPermission($account, "use $workflow_id transition $transition_id"));
+      } else {
+        $access = $object->access('update', $account, TRUE);
+      }
+      
       return $return_as_object ? $access : $access->isAllowed();
     }
 
-    // Other entity types may have different
-    // access methods and properties.
+    // Other entity types may have different access methods and properties.
     return ($return_as_object ? AccessResult::allowed() : true );
   }
 }
